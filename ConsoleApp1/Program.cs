748a29086aa999845413627edccd4b9167c7b519
`Vlad` <86572964+Hikfound@users.noreply.github.com>
2023-03-16 12:20:53 +0200

    версия 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,18 +6,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-                //Нахождение максимального элемента и его индекса
-                int[] array = { 8, -1, 4, 16, -5, 3 };
-                int max = array[0]; int indexMax = 0;
-                for (int i = 1; i < array.Length; i++)
-                {
-                    if (array[i] > max)
-                    {
-                        max = array[i]; indexMax = i;
-                    }
-                    Console.WriteLine($"max={max}\t{indexMax}");
-                }
-                Console.Read();
+            //Нахождение максимального элемента и его индекса
+            int[] array = { 8, -1, 4, 16, -5, 3 };
+            int max = array[0]; int indexMax = 0;
+            int min = array[0]; int indexMin = 0;
+            for (int i = 1; i < 6; i++)
+            {
+            if (array[i] > max)
+            {
+                max = array[i]; indexMax = i;
             }
+            if (array[i] < min)
+            {
+                min = array[i]; indexMin = i;
+            }
+            }
+            Console.WriteLine($"min={min}\tindex={indexMin}");
+            Console.WriteLine($"max={max}\tindex={indexMax}");
+            Console.Read();
+        }
     }
 }
